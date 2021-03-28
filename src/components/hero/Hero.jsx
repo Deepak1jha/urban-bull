b1f21abd2d3f1056419b48deb6d813ffe98ca645
Deepak1jha <ddjha171@gmail.com>
2021-03-28 11:51:30 +0530

    Navbar component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,68 @@
+import styled from 'styled-components';
+import bgImage from '../../assets/bg-image.png';
+
+const Section = styled.section`
+  background-image: url(${bgImage});
+  height: 787px;
+  display: block;
+  background-size: contain;
+  background-repeat: no-repeat;`;
+
+const Content = styled.div`
+  width: 100%;
+  height: 100px;`;
+const Left = styled.div`
+  padding-left: 220px;
+  padding-top: 143px
+`;
+const Title = styled.p`
+  font-size: 55px;
+  color: #04050a;
+  font-weight: 400;
+`;
+const Desc = styled.p`
+  width: 472px;
+  font-size: 20px;
+  color: #9ea0ac;
+  line-height: 30px;
+  margin-top: 58px;
+`;
+const Button = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 18px;
+  margin-top: 58px;
+  width: 371px;
+  height: 71px;
+  line-height: 71px;
+  font-size: 22px;
+  text-align: center;
+  color: #fff;
+  cursor: pointer;
+  background: linear-gradient(90deg, #0546d6, #3f89fc);
+  text-decoration: none;
+  box-shadow: 0 15px 14px rgb(0 42 177 / 12%);`;
+
 const Hero = () => {
     return (
-        <div>
-            <h2>sadadasdas</h2>
-        </div>
+        <Section>
+            <Content>
+                <Left>
+                    <Title>
+                        Get 2 free Website at price 1
+                    </Title>
+                    <Desc>
+                        Open and fund a developer to build a good web application with under 1000$
+                        and also have a chance to get 15% discount on <span>React,Vue</span>{' '}
+                        and more!
+                        <Button href={"https://github.com/Deepak1jha"} target="_blank">
+                            <span>Claim your discount now</span>
+                        </Button>
+                    </Desc>
+                </Left>
+            </Content>
+        </Section>
     )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,69 +1,127 @@
-import styled from 'styled-components';
-import bgImage from '../../assets/bg-image.png';
+import React from 'react';
+import styled from 'styled-components/macro';
+import Img from '../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
-const Section = styled.section`
-  background-image: url(${bgImage});
-  height: 787px;
-  display: block;
-  background-size: contain;
-  background-repeat: no-repeat;`;
-
-const Content = styled.div`
+const NavbarContainer = styled.div`
   width: 100%;
-  height: 100px;`;
-const Left = styled.div`
-  padding-left: 220px;
-  padding-top: 143px
+  height: 80px;
+  z-index: 20;
+  background: #fff;
 `;
-const Title = styled.p`
-  font-size: 55px;
-  color: #04050a;
-  font-weight: 400;
-`;
-const Desc = styled.p`
-  width: 472px;
-  font-size: 20px;
-  color: #9ea0ac;
-  line-height: 30px;
-  margin-top: 58px;
-`;
-const Button = styled.a`
+
+const NavbarWrap = styled.div`
+  width: 1200px;
+  height: 100%;
+  margin: 0 auto;
+  z-index: 20;
   display: flex;
-  justify-content: center;
+  position: relative;
   align-items: center;
-  border-radius: 18px;
-  margin-top: 58px;
-  width: 371px;
-  height: 71px;
-  line-height: 71px;
-  font-size: 22px;
-  text-align: center;
+  justify-content: space-between;
+`;
+const Logo = styled.div`
+  width: 128px;
+  height: 22px;
+  background: url(${Img}) 0 45% no-repeat;
+  background-size: 100% 100%;
+  cursor: pointer;
+`;
+
+const Nav = styled.nav`
+  flex: 1;
+  position: relative;
+  padding-left: 50px;
+`;
+
+const NavLink = styled(Link)`
+  color: #000;
+  padding: 0 15px;
+  font-size: 16px;
+  line-height: 80px;
+  font-weight: 700;
+  text-decoration: none;
+`;
+
+const Search = styled.div`
+  right: 235px;
+  position: absolute;
+`;
+
+const SearchWrap = styled.div`
+  width: 220px;
+  height: 36px;
+  position: relative;
+  box-sizing: border-box;
+  background: rgba(179, 191, 201, 0.15);
+  line-height: 33px;
+  padding-left: 37px;
+  border-radius: 19px;
+`;
+
+const Input = styled.input`
+  color: #000;
+  width: 196px;
+  border: transparent;
+  font-size: 12px;
+  background: transparent;
+  outline: none;
+  &::placeholder {
+    color: #d1d8de;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Button = styled.div`
+  width: 110px;
   color: #fff;
   cursor: pointer;
-  background: linear-gradient(90deg, #0546d6, #3f89fc);
-  text-decoration: none;
-  box-shadow: 0 15px 14px rgb(0 42 177 / 12%);`;
+  height: 36px;
+  font-size: 16px;
+  box-sizing: border-box;
+  background: #007cff;
+  text-align: center;
+  line-height: 36px;
+  border-radius: 21px;
+`;
 
 const Navbar = () => {
     return (
-        <Section>
-            <Content>
-                <Left>
-                    <Title>
-                        Get 2 free
-                    </Title>
-                    <Desc>
-                        Open and fund a developer to build a good web application with under 1000$
-                        and also have a chance to get 15% discount on <span>React,Vue</span>{' '}
-                        and more!
-                        <Button href={"https://github.com/Deepak1jha"} target="_blank">
-                            <span>Claim your discount now</span>
-                        </Button>
-                    </Desc>
-                </Left>
-            </Content>
-        </Section>
-    )
-}
+        <NavbarContainer>
+            <NavbarWrap>
+                <Logo />
+                <Nav>
+                    <NavLink to='/'>HOME</NavLink>
+                    <NavLink to='/'>MARKET</NavLink>
+                    <NavLink to='/'>TRADE</NavLink>
+                    <NavLink to='/'>PRICING</NavLink>
+                    <NavLink to='/'>DOWNLOAD</NavLink>
+                    <NavLink to='/'>HELP</NavLink>
+                </Nav>
+                <Search>
+                    <SearchWrap>
+                        <Input type='text' placeholder='Symbol/Name' />
+                    </SearchWrap>
+                </Search>
+                <ButtonContainer>
+                    <Button
+                        css={`
+              color: #037cff;
+              background: #fff;
+            `}
+                    >
+                        SIGN UP
+                    </Button>
+                    <Button>LOG IN</Button>
+                </ButtonContainer>
+            </NavbarWrap>
+        </NavbarContainer>
+    );
+};
 
 export default Navbar;
